9740caf79a2c6b494a78d0e80b6c1003c279ebea
Emre Torun <emre.torun@trendyol.com>
2018-05-29 11:23:10 +0300

    updates

--- src/locustor_config.py
+++ src/locustor_config.py
@@ -7,7 +7,7 @@ class LocustorConfig:
     INFLUXDB_PORT = '8086'
     INFLUXDB_USER_NAME = ''
     INFLUXDB_PASSWORD = ''
-    INFLUXDB_DATABASE = ''
+    INFLUXDB_DATABASE = 'LOCUST_LOG'
     DELAY_IN_SEC = 5
     LOCUST_CSV_LOG_FILE_PATH = '/home/emre/testresult_requests.csv'
     HOST = ''
